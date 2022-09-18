40034f3b471f3b9d2d70d70a5d8e117d268dab68
tuddev <bubnika23@gmail.com>
2022-09-18 21:21:10 +0500

    feat(app): add routing

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { AuthorisationPage } from "../AuthorisationPage";
 import { ContactsPage } from "../ContactsPage";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 export function App() {
-  return (
-    <>
-      <AuthorisationPage />
-      <ContactsPage />
-    </>
-  );
+  return  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<ContactsPage />} />
+      <Route path="/login" element={<AuthorisationPage />}>
+      </Route>
+    </Routes>
+  </BrowserRouter>
 }
