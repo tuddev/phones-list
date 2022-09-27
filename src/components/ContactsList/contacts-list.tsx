a498b4d5a7f62c78d82542d9a7944feeb8573116
tuddev <bubnika23@gmail.com>
2022-09-27 23:39:58 +0500

    feat(contacts): add contacts list info messages

--- src/components/ContactsList/contacts-list.tsx
+++ src/components/ContactsList/contacts-list.tsx
@@ -1,27 +1,42 @@
-import { Container, List } from '@mui/material';
+import { Container, List, Typography } from '@mui/material';
 import { observer } from 'mobx-react-lite';
 import React, { useEffect } from 'react';
 import { ContactModel, contactsService, loginStore } from '../../services';
 import { contactsStore } from '../../stores';
 import { ContactItem } from '../ContactItem';
 
-
 export const ContactsList: React.FC = observer(() => {
   useEffect(() => {
     contactsService.init(loginStore.getUser()?.id);
     contactsStore.init();
   }, []);
-  
+
+  const isNoContacts = contactsStore.filteredContacts.length < 1;
+  const isSearch = contactsStore.searchQuery.length > 0;
+
+  if (isNoContacts && isSearch) {
+    return (
+      <Container>
+        <Typography>Контактов не найдено</Typography>
+      </Container>
+    );
+  }
+
+  if (isNoContacts) {
+    return (
+      <Container>
+        <Typography>Вы не добавили ни один контакт</Typography>
+      </Container>
+    );
+  }
+
   return (
     <Container>
       <List>
-        {
-          contactsStore.filteredContacts.map((contact: ContactModel) => (
-            <ContactItem key={contact.id} contact={contact}/>
-          ))
-        }
+        {contactsStore.filteredContacts.map((contact: ContactModel) => (
+          <ContactItem key={contact.id} contact={contact} />
+        ))}
       </List>
     </Container>
   );
 });
-
